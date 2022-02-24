6e0b93ca9d6f432f9577ad61cf22f90e8493e05e
Tirumal Reddy <Tirumal.Reddy@lumen.com>
2022-02-23 21:48:28 -0600

    Added RabbitMQ test code

--- Controllers/RabbitMQTestController.cs
+++ Controllers/RabbitMQTestController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace rabbitmq_test.Controllers
@@ -21,6 +23,24 @@ namespace rabbitmq_test.Controllers
         [HttpGet]
         public string Get()
         {
+            var factory = new ConnectionFactory() { HostName = "rabbitmq-swift-ft-dev.apps.ddc-test.corp.intranet" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                      durable: false,
+                                      exclusive: false,
+                                      autoDelete: false,
+                                      arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+            }
             return "Success";
         }
     }
